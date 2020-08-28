7ef00f72f8d3cbcd7f0058167438b45fdf192594
Fran Vallejo <neotherack@gmail.com>
2020-08-28 14:21:52 +0200

    added sepparate commands and intents

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 ask = Ask(app, "/")
 
 sitios_fran = {'oficina', 'de fran', 'habitación'}
-id_fran = "amzn1.ask.account.AFINAOB5SJS7H4ZIU4WMAPU3WARKM62SJH2WAS6KBZBGRD4SE3NVDHT67IJOAZDLRUTHYFU3D5GDIQQLK27C4KZAYKSMXBAIEDPWCTXGF5TD5ZRRM7IYP3PVAC4X33OEZ4V7PZ2GIZB446CP4QDZBC3XAMQQVZD2S3XERWQWTZJIUIAJHYXYLAGDQTFVXGJNITJYBRNIYH7V2UY"
+id_fran  = "amzn1.ask.account.AFINAOB5SJS7H4ZIU4WMAPU3WARKM62SJH2WAS6KBZBGRD4SE3NVDHT67IJOAZDLRUTHYFU3D5GDIQQLK27C4KZAYKSMXBAIEDPWCTXGF5TD5ZRRM7IYP3PVAC4X33OEZ4V7PZ2GIZB446CP4QDZBC3XAMQQVZD2S3XERWQWTZJIUIAJHYXYLAGDQTFVXGJNITJYBRNIYH7V2UY"
 sitios_ester = {'salón', 'de ester'}
-id_ester = ""
+id_ester = "amzn1.ask.account.AHJWELXVQRIEFFKDTMDOHFIRCF6I3IMKWHVPMLT43U3MVFCHAC2V4JBJK5JXHJI47IQBAVTSP2Z2I2Y665FUNBIOY6X3UHLS63V7EBJ6K3SMFZ7K5MFUU2NVBOCPSM6YLTJRX4PK7YFKSWL3FUD5UKRSB3MJ6SVHJUVF7EMBNZWW2MOJOCSHFBSTUKDZE2BZURFPW7IAH4P5L6Y"
 
 
 @ask.launch
@@ -41,29 +41,34 @@ def ester_wol():
 
 @ask.intent("WOLIntent", mapping={'verbo':'verbo', 'cosa':'cosa', 'sitio':'sitio'})
 def wol(verbo, cosa, sitio):
+  usuario = user_name(session.user.userId)
+
+  print("sitio: %s" % sitio)
+  print("usuario: %s" % usuario)
+
   if (sitio in sitios_fran):
-    fran_wol()
+    return fran_wol()
 
-  if (sitio in sitios_ester):
-    ester_wol()
+  elif (sitio in sitios_ester):
+    return ester_wol()
 
-  if (sitio is None):
-    if (user_name(session.user.userId)=='Fran'):
-      fran_wol()
-    if (user_name(session.user.userId)=='Ester'):
-      ester_wol()
-    return statement('No he reconocido la voz de la persona que habla')
-
-  return question('No te he entendido bien, por favor, inténtalo de nuevo')
+  else:
+    return question('¡Caquita fresca! he entendido %s y %s' % (usuario, sitio))
 
 
 @ask.intent("MyWOLIntent", mapping={'verbo':'verbo', 'cosa':'cosa'})
-def wol(verbo, cosa):
-  if (user_name(session.user.userId)=='Fran'):
-    fran_wol()
-  if (user_name(session.user.userId)=='Ester'):
-    ester_wol()
-  return statement('No he reconocido la voz de la persona que habla')
+def mywol(verbo, cosa):
+  usuario = user_name(session.user.userId)
+  print("usuario: %s" % usuario)
+
+  if (usuario == "Fran"):
+    return fran_wol()
+
+  elif (usuario == "Ester"):
+    return ester_wol()
+
+  else:
+    return statement('¡Putísimo Coque! He entendido %s' % usuario)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5443)
