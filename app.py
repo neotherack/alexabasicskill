ed14723b8b8d4c84331e60c0e0667dce91845703
Fran Vallejo <neotherack@gmail.com>
2020-08-29 21:24:36 +0200

    moved ssh stuff to ssh lib, now app file is cleaner

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_ask import Ask, statement, question, session
 from wakeonlan import send_magic_packet
-import paramiko
 import logging
 import configparser
-import json
+
+from lib.ssh import *
 
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
@@ -31,29 +31,6 @@ def user_name(id):
     return "Fran"
   else: #amzids['id_ester']
     return "Ester"
-
-def ssh_command(hostname='localhost', port='22', username='user', password='NaN', command='dir /'):
-
-  ret = ""
-  err = ""
-
-  try:
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy)
-
-    client.connect(hostname, port=port, username=username, password=password)
-
-    stdin, stdout, stderr = client.exec_command(command)
-    ret = stdout.read().decode('utf-8')
-    err = stderr.read().decode('utf-8')
-
-    return [ret,err]
-
-  except Exception as e:
-    return ["",str(e)]
-  finally:
-    client.close()
 
 def fran_wol():
   try:
---
+++ lib/ssh.py
@@ -0,0 +1,24 @@
+import paramiko
+
+def ssh_command(hostname='localhost', port='22', username='user', password='NaN', command='dir /'):
+
+  ret = ""
+  err = ""
+
+  try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.WarningPolicy)
+
+    client.connect(hostname, port=port, username=username, password=password)
+
+    stdin, stdout, stderr = client.exec_command(command)
+    ret = stdout.read().decode('utf-8')
+    err = stderr.read().decode('utf-8')
+
+    return [ret,err]
+
+  except Exception as e:
+    return ["",str(e)]
+  finally:
+    client.close()
