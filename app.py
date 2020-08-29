07856149619b5345f24e9dc37b34451ec158aa51
Fran Vallejo <neotherack@gmail.com>
2020-08-29 20:32:06 +0200

    config settings added

--- app.py
+++ app.py
@@ -3,23 +3,37 @@ from flask_ask import Ask, statement, question, session
 from wakeonlan import send_magic_packet
 import paramiko
 import logging
+import configparser
+import json
 
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-sitios_fran = {'oficina', 'de fran', 'habitación'}
-id_fran = "amzn1.ask.account.AHJWELXVQRIEFFKDTMDOHFIRCF6I3IMKWHVPMLT43U3MVFCHAC2V4JBJK5JXHJI47IQBAVTSP2Z2I2Y665FUNBIOY6X3UHLS63V7EBJ6K3SMFZ7K5MFUU2NVBOCPSM6YLTJRX4PK7YFKSWL3FUD5UKRSB3MJ6SVHJUVF7EMBNZWW2MOJOCSHFBSTUKDZE2BZURFPW7IAH4P5L6Y"
-sitios_ester = {'salón', 'de ester'}
-id_ester = "amzn1.ask.account.AHJWELXVQRIEFFKDTMDOHFIRCF6I3IMKWHVPMLT43U3MVFCHAC2V4JBJK5JXHJI47IQBAVTSP2Z2I2Y665FUNBIOY6X3UHLS63V7EBJ6K3SMFZ7K5MFUU2NVBOCPSM6YLTJRX4PK7YFKSWL3FUD5UKRSB3MJ6SVHJUVF7EMBNZWW2MOJOCSHFBSTUKDZE2BZURFPW7IAH4P5L6Y"
+config = configparser.ConfigParser()
+config.read('settings.conf')
 
-arranque = {'arranca', 'inicia', 'lanza'}
-parada = {'detén', 'para', 'mata'}
+serv = config['Main-Settings']
+sshconn = config['SSH-Connection']
+sshcom  = config['SSH-Commands']
+places = config['PC-Places']
+actions = config['MC-Actions']
+amzids = config['IDs-Amazon']
+macs = config['MAC']
 
-cmdArrancarMC = "/opt/minecraft/minecraft_server/launch.sh"
-cmdGetPIDMC = "ps -fea | grep mine | grep java | grep -v grep | awk '{print $2}'"
-cmdPararMC = "kill -9 $(%s)" % cmdGetPIDMC
+sitios_fran = places['sitios_fran']
+sitios_ester = places['sitios_ester']
+
+id_fran = amzids['id_fran']
+id_ester = amzids['id_ester']
+
+arranque = actions['arranque']
+parada = actions['parada']
+
+cmdArrancarMC = sshcom['cmdArrancarMC']
+cmdGetPIDMC = sshcom['cmdGetPIDMC']
+cmdPararMC = sshcom['cmdPararMC']
 
 @ask.launch
 def start_skill():
@@ -41,9 +55,7 @@ def ssh_command(hostname='localhost', port='22', username='user', password='NaN'
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.WarningPolicy)
 
-    print("debug: %s %s %s %s" % (hostname, port, username, password))
     client.connect(hostname, port=port, username=username, password=password)
-    print("command: %s" % command)
 
     stdin, stdout, stderr = client.exec_command(command)
     ret = stdout.read().decode('utf-8')
@@ -53,33 +65,28 @@ def ssh_command(hostname='localhost', port='22', username='user', password='NaN'
 
   except Exception as e:
     return ["",str(e)]
-    #print('error en la conexión ssh %s' % str(e))
   finally:
     client.close()
 
 def fran_wol():
   try:
-    send_magic_packet('00.e0.4c.68.0d.4c')
+    send_magic_packet(macs['fran'])
     return statement('Se ha enviado la orden al PC de la oficina')
   except:
     return statement('No he podido arrancar el ordenador de la oficina')
 
 def ester_wol():
   try:
-    send_magic_packet('70.8b.cd.27.e1.dd')
+    send_magic_packet(macs['ester'])
     return statement('Se ha enviado la orden al portátil de Ester')
   except:
     return statement('No he podido arrancar el portátil de Ester')
 
 def enviar_cmd(cmd):
   try:
-    stdout, stderr = ssh_command(hostname='putisimocoque.tk', port='1986', username='fran', password='matrix05', command=cmd)
-
-    print("stdout: %s" % stdout.strip())
-    print("stderr: %s" % stderr.strip())
+    stdout, stderr = ssh_command(hostname=sshconn['hostname'], port=sshconn['port'], username=sshconn['username'], password=sshconn['password'], command=cmd)
 
     return [stdout.strip(), stderr.strip()]
-#    return ['1999','']
   except Exception as e:
     return ["", str(e)]
 
@@ -97,9 +104,6 @@ def revisar_servidor():
 def wol(verbo, cosa, sitio):
   usuario = user_name(session.user.userId)
 
-#  print("sitio: %s" % sitio)
-#  print("usuario: %s" % usuario)
-
   if (sitio in sitios_fran):
     return fran_wol()
 
@@ -113,7 +117,6 @@ def wol(verbo, cosa, sitio):
 @ask.intent("MyWOLIntent", mapping={'verbo':'verbo', 'cosa':'cosa'})
 def mywol(verbo, cosa):
   usuario = user_name(session.user.userId)
-#  print("usuario: %s" % usuario)
 
   if (usuario == "Fran"):
     return fran_wol()
@@ -128,7 +131,6 @@ def mywol(verbo, cosa):
 @ask.intent("MCStartStopIntent", mapping={'instruccion':'instruccion'})
 def mcstartstop(instruccion):
   usuario = user_name(session.user.userId)
-#  print("usuario: %s" % usuario)
 
   pid, err = revisar_servidor()
 
@@ -151,12 +153,8 @@ def mcstartstop(instruccion):
 @ask.intent("MCStatusIntent")
 def mcstatus():
   usuario = user_name(session.user.userId)
-#  print("usuario: %s" % usuario)
 
   pid, err = revisar_servidor()
-
-  print("pid %s" % pid)
-  print("err %s" % err)
 
   if (pid is not None): #si está arrancado
     return statement('El servidor de Minecraft está arrancado con el PID: %s' % pid)
@@ -165,4 +163,4 @@ def mcstatus():
     return statement('El servidor de Minecraft está parado')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5443)
+  app.run(debug=True, host=serv['host'], port=serv['port'])
